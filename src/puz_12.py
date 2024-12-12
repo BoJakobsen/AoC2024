956ed9e7f06b413a141f2c5a49b86e1af7c4351d
Bo Jakobsen <boj@ruc.dk>
2024-12-12 21:36:38 +0100

    Day 12, Cleaned up code (not in any way optimised)

--- src/puz_12.py
+++ src/puz_12.py
@@ -1,15 +1,16 @@
-#Snippets is loaded before running this
+# Snippets.py is loaded before running this
+# Load packages and defines a few help functions
 
-#lines = baseloader('../data/12_test.dat')
+# lines = baseloader('../data/12_test.dat')
 lines = baseloader('../data/12.dat')
 
-
+# Organise data in dict after type
 dat = defaultdict(list)
 for Nll, ll in enumerate(lines):
     for Ncc, cc in enumerate(ll):
         dat[cc].append((Nll,Ncc))
 
-# get all nab points, within bound
+# get all neighbour points, within boundary
 def get_nab(coord):
     dirs=[(1,0), (0,1), (-1,0), (0,-1)]
     nabs=[]
@@ -18,7 +19,7 @@ def get_nab(coord):
             nabs.append(tupadd(d,coord))
     return nabs
 
-# get all nab points, ignoring bound
+# get all neighbour points, ignoring boundary
 def get_nab2(coord):
     dirs=[(1,0), (0,1), (-1,0), (0,-1)]
     nabs=[]
@@ -26,24 +27,19 @@ def get_nab2(coord):
         nabs.append(tupadd(d,coord))
     return nabs
 
-
-def is_nab(coord,coord0):
-    return coord in get_nab(coord0)
-
-
-# Find the distinct connected regions
+# Find the distinct connected regions, and organise in new structure
 groups = defaultdict(list)
 for kind in dat.keys():
     poss = copy.copy(dat[kind])
     while len(poss)>0:
-        grp = [poss[0]]  # add first poss to the grp
+        grp = [poss[0]]  # add first position to the group
         poss.remove(grp[0])  # remove from list
         added = True
         while added:
             added = False 
             for pos in poss:  # check if we can add a number
                 nab = get_nab(pos)
-                if set(nab).intersection(set(grp)):  # is nab a nabou
+                if set(nab).intersection(set(grp)):  # is nab a neighbour
                     grp.append(pos)
                     poss.remove(pos)
                     added = True
@@ -57,11 +53,11 @@ for regs in groups.values():
         ares = len(reg)
         per = 0
         for point in reg:
-            per += 4-len(set(get_nab2(point)).intersection(set(reg)))
+            per += 4-len(set(get_nab2(point)).intersection(set(reg)))  # number of edges
         res += ares * per
 print(res)
 
-# Helper fuction
+# Find square boundaries of a connected region
 def minmax(reg):
     minl = len(lines)
     maxl = 0
@@ -77,54 +73,45 @@ def minmax(reg):
         if p[1] > maxc :
             maxc = p[1]
     return minl,maxl,minc,maxc
-            
-        
-    
-
+ 
 
 # Problem 2
 
+# For each connected region scan along vertical and horizontal lines and
+# count edge, not very compact/optimised solution, but the algorithm works
 res = 0
 for key , regs in groups.items():
     for reg in regs:
         bound = minmax(reg) # line char
         sides = 0
-        # count horizontal
-        for l in range(bound[0]-2,bound[1]+3):
+        # Count horizontal edge
+        for l in range(bound[0]-1,bound[1]+2):
             ingrp_down = False
             ingrp_up = False
-            for c in range(bound[2]-1,bound[3]+2):
+            for c in range(bound[2],bound[3]+1):
                 if not ingrp_down and (l+1,c) in reg and (l,c) not in reg: 
                     ingrp_down = True
-        #            print("start")
-        #            print(l,c)
                     sides += 1
                 if ingrp_down and ((l+1,c) not in reg or (l,c)  in reg): 
                     ingrp_down = False
                 if not ingrp_up and (l-1,c) in reg and (l,c) not in reg: 
                     ingrp_up = True
-        #            print("start")
-        #            print(l,c)
                     sides += 1
                 if ingrp_up and ((l-1,c) not in reg or (l,c)  in reg): 
                     ingrp_up = False
 
-        # count vertical
-        for c in range(bound[2]-2,bound[3]+3):
+        # Count vertical edge
+        for c in range(bound[2]-1,bound[3]+2):
             ingrp_left = False
             ingrp_right = False
-            for l in range(bound[0]-1,bound[1]+2):
+            for l in range(bound[0],bound[1]+1):
                 if not ingrp_right and (l,c+1) in reg and (l,c) not in reg: 
                     ingrp_right = True
-        #            print("start")
-        #            print(l,c)
                     sides += 1
                 if ingrp_right and ((l,c+1) not in reg or (l,c)  in reg): 
                     ingrp_right = False
                 if not ingrp_left and (l,c-1) in reg and (l,c) not in reg: 
                     ingrp_left = True
-        #            print("start")
-        #            print(l,c)
                     sides += 1
                 if ingrp_left and ((l,c-1) not in reg or (l,c)  in reg): # end of grp
                     ingrp_left = False
