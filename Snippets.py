a3a31416b3764d1e030dcd97c6b7c2bc463058b1
Bo Jakobsen <boj@ruc.dk>
2024-12-30 12:32:05 +0100

    Snippets file cleaned up

--- Snippets.py
+++ Snippets.py
@@ -1,22 +1,25 @@
 # Some usefully stuff for working with AoC problems
-# Potentially nice to  just run before slowing
+# Potentially nice to  just run before solving
 
-# Libs often used
+# Libraries very often used (should properly just be loaded always)
 import numpy as np
-import itertools
 import re
+import copy
+
 from collections import defaultdict
 from collections import deque
 
-from functools import cache 
+#from functools import cache 
 # add @cache before eg recursive functions, very efficient. 
 
-import copy
+#import itertools # # for e.g. permutations
 
+#import networkx as nx # for graphs, is a large library
 
-import networkx as nx # for graphs
+    
+##  Some functions 
 
-# basic loader
+# Basic loader
 def baseloader(filename):
     with open(filename) as f:
         lines = [(x.strip()) for x in f]
@@ -51,44 +54,71 @@ def maptodict(lines, basechar):
     return pos
 
 
-## from day 23,  finde the maximal clique in a graph
-#G = nx.Graph()
-#for line in lines:
-#    a ,b = line.split('-')
-#    G.add_edge(a,b)
-#','.join(sorted(max(nx.find_cliques(G), key=len)))
+## Some snippets that can maybe be usefull
 
 
+## Some loader snippets
+
+# lines in a list
+# with open(filename) as f:
+#     lines = [(x.strip()) for x in f]
 
 
-# Links to other AoC resources
+# Flexible e.g. for parts separated with blank line
+# with open('data/24_test.dat') as f:
+#     a = f.read().strip()
+# lines, gates = a.split('\n\n')
+# for line in lines.split('\n'):
+#     a,b = line.split(': ')
 
-# Nice list of python snippets from AoC
-# https://www.reddit.com/r/adventofcode/comments/1gsl4fm/share_your_favorite_tricks_and_snippets/
 
-# from https://github.com/hyperneutrino/advent-of-code/
-# nice tricks  there.
-#for line in open(0):
-#    robots.append(tuple(map(int, re.findall(r"-?\d+", line))))
-
-# break at a empty line, for two 
-#for line in file:
+# break at a empty line, for two parts
+# for line in file:
 #    if line.isspace(): break
 #    rules.append(list(map(int, line.split("|"))))
 
 
-    
-# horizontal slicing is easy
-#map[2][3:]
+# For finding number I guess (from Hyperneutrino)
+#for line in open(0):
+#    robots.append(tuple(map(int, re.findall(r"-?\d+", line))))
 
-# vertically slicing
+
+## Graph stuff
+
+# Find the maximal clique in a graph, from day 23,  
+# G = nx.Graph()
+# for line in lines:
+#    a ,b = line.split('-')
+#    G.add_edge(a,b)
+# ','.join(sorted(max(nx.find_cliques(G), key=len)))
+
+
+## Slicing    
+
+# Horizontal slicing is easy
+# map[2][3:]
+
+# Vertically slicing
 # "".join([a[2] for a in map[3:]  ])
 # is a bit not so nice
 
 # Numpy arrays are much easier to slice.
- #a= np.array(map)
- #b = a[:,1]  # by reference ! so we can just assign
- #ind=np.where(b=='O')
- #b[ind] = 'X'
- #a
- # This is very nice for this.  
+# a= np.array(map)
+# b = a[:,1]  # by reference ! so we can just assign
+# ind=np.where(b=='O')
+# b[ind] = 'X'
+
+
+## Links to other AoC resources
+
+# Nice list of python snippets from AoC
+# https://www.reddit.com/r/adventofcode/comments/1gsl4fm/share_your_favorite_tricks_and_snippets/
+
+# Nice post, will all old AoC problems
+# https://www.reddit.com/r/adventofcode/comments/1gdw4cj/450_stars_a_categorization_and_megaguide/
+
+
+# Hyperneutrino, vvery nice solutions in Python and god youtube videos
+# https://github.com/hyperneutrino/advent-of-code/
+
+
